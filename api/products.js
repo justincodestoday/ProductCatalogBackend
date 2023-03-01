d369fd5a0187a6c29e9be1e7b9ce24fee87cba29
juju <jcmc888@gmail.com>
2023-03-01 10:34:07 +0800

    first commit

--- api/products.js
+++ api/products.js
@@ -1,52 +1,95 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { findByIdAndDelete, findByIdAndUpdate } = require("../models/Product");
 const router = express.Router();
 const Product = require("../models/Product");
 
 //get products
 router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find();
-    return res.json(products);
-  } catch {
-    return res.status(500).json({ message: "Server Error" });
-  }
+    try {
+        const products = await Product.find();
+        return res.json(products);
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
+//get product by id
+router.get("/:id", async (req, res) => {
+    try {
+        const productFound = await Product.findById(req.params.id);
+        if (!productFound)
+            return res.status(404).json({ message: "Product not found" });
+        else return res.json(productFound);
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+//add product
 router.post("/", async (req, res) => {
-  try {
-    const {
-      title,
-      brand,
-      category,
-      description,
-      price,
-      discountPercentage,
-      rating,
-      stock,
-      thumbnail,
-    } = req.body;
+    try {
+        const {
+            title,
+            brand,
+            category,
+            description,
+            price,
+            discountPercentage,
+            rating,
+            stock,
+            thumbnail,
+        } = req.body;
 
-    const product = new Product({
-      title,
-      brand,
-      category,
-      description,
-      price,
-      discountPercentage,
-      rating,
-      stock,
-      thumbnail,
-    });
+        const product = new Product({
+            title,
+            brand,
+            category,
+            description,
+            price,
+            discountPercentage,
+            rating,
+            stock,
+            thumbnail,
+        });
 
-  
-    await product.save();
-    return res
-      .status(200)
-      .json({ product, msg: "New product added successfully" });
-  } catch {
-    return res.status(500).json({ message: "Server Error" });
-  }
+        await product.save();
+        return res
+            .status(200)
+            .json({ product, msg: "New product added successfully" });
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+//update product by id
+router.put("/:id", async (req, res) => {
+    try {
+        const productFound = await Product.findById(req.params.id);
+        if (!productFound)
+            return res.status(404).json({ message: "Product not found" });
+
+        await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.json({
+            productFound,
+            message: "Product updated successfully",
+        });
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
+});
+
+//delete product by id
+router.delete("/:id", async (req, res) => {
+    try {
+        const productFound = await Product.findById(req.params.id);
+        if (!productFound)
+            return res.status(404).json({ message: "Product not found" });
+        await Product.findByIdAndDelete(req.params.id);
+        return res.json({ message: "Product deleted successfully" });
+    } catch {
+        return res.status(500).json({ message: "Server Error" });
+    }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 
-mongoose.connect(process.env.MONGO_DB)
+mongoose.connect(process.env.MONGO_DB);
 
 app.use(express.static("uploads"));
-
 
 app.use(cors());
 app.use(express.json());
@@ -17,5 +16,5 @@ app.use("/products", require("./api/products"));
 app.listen(5000, () => console.log("App is flying on PORT 5000"));
 
 mongoose.connection.once("open", () =>
-  console.log("We are connected to MongoDB")
+    console.log("We are connected to MongoDB")
 );
